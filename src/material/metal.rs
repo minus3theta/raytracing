5781043b78929507edf891852c6afa5f5f9cc9c1
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-03-06 16:33:46 +0900

    Implement fuzzy reflection (Listing 52)

--- src/material/metal.rs
+++ src/material/metal.rs
@@ -1,20 +1,25 @@
-use crate::{Color, HitRecord, Material, Random, Ray};
+use crate::{Color, HitRecord, Material, Random, Ray, Vec3};
 
 #[derive(Debug, PartialOrd, PartialEq, Clone, Default)]
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Self { albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Self {
+        let fuzz = if fuzz < 1.0 { fuzz } else { 1.0 };
+        Self { albedo, fuzz }
     }
 }
 
 impl Material for Metal {
-    fn scatter(&self, r_in: &Ray, rec: &HitRecord, _rng: &mut Random) -> Option<(Color, Ray)> {
+    fn scatter(&self, r_in: &Ray, rec: &HitRecord, rng: &mut Random) -> Option<(Color, Ray)> {
         let reflected = r_in.dir.unit_vector().reflect(&rec.normal);
-        let scattered = Ray::new(rec.p.clone(), reflected);
+        let scattered = Ray::new(
+            rec.p.clone(),
+            reflected + self.fuzz * Vec3::random_in_unit_sphere(rng),
+        );
         if scattered.dir.dot(&rec.normal) > 0.0 {
             Some((self.albedo.clone(), scattered))
         } else {
